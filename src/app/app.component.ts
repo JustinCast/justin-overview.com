ce3d77791a6206d4867ed99d5ea0677c6630a205
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-10-06 19:14:36 -0600

    feat: se sobreescribe correctamente el color del mat-accordion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'JustinOverview';
+
+  ngAfterViewInit() {
+    
+    let style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.background { background-color: #28292a; padding-top: 0.5%; color: white}';
+    document.getElementsByTagName('head')[0].appendChild(style);
+    let ul = document.getElementsByClassName("mat-expansion-panel-content")
+    ul[0].classList.add('background')
+  }
 }
