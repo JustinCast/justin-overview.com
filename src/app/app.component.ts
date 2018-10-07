c2a9ad9d8d119a750aca6766a58f0bc226ed9cb3
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-10-06 22:59:04 -0600

    fix: some styles errors fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  title = 'JustinOverview';
+export class AppComponent implements AfterViewInit, OnInit {
+  year;
 
+  ngOnInit() {
+    var d = new Date();
+    this.year = d.getFullYear();
+  }
   ngAfterViewInit() {
     
     let style = document.createElement('style');
