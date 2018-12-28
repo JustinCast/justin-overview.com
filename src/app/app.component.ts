1b0bb35699ec5453f9bf73c6f33dba258dde9e89
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-12-27 21:56:23 -0600

    feta: some icons added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,23 @@ export class AppComponent implements AfterViewInit, OnInit {
   year;
   pdf: string = "../assets/cv.pdf";
   collapsed = true;
+  technologies = {
+    Angularjs: "../assets/",
+    Nodejs: "",
+    "Angular 2+": "",
+    Mongo: "",
+    "MSSQL Server": "",
+    "PostgreSQL": "",
+    "Bootstrap": "",
+    "Angular Material": "",
+    "GraphQL": "",
+    "Apollo": "",
+    "C#": "",
+    "Java": "",
+    "JavaScript": "",
+    "C": "",
+    "C++": "",
+  };
 
   constructor(private _api: ApiManagementService) {}
 
@@ -21,23 +38,7 @@ export class AppComponent implements AfterViewInit, OnInit {
     var d = new Date();
     this.year = d.getFullYear();
   }
-  ngAfterViewInit() {
-    // let style = document.createElement("style");
-    // style.type = "text/css";
-    // style.innerHTML =
-    //   ".background { background-color: #444344; padding-top: 0.5%; color: white}";
-    // document.getElementsByTagName("head")[0].appendChild(style);
-    // let ul = document.getElementsByClassName("mat-expansion-panel-content");
-    // ul[0].classList.add("background");
-
-    // // mat-tab-label-container
-    // let tabHeader = document.createElement("style");
-    // tabHeader.type = "text/css";
-    // tabHeader.innerHTML = ".tab { background-color: white}";
-    // document.getElementsByTagName("head")[0].appendChild(style);
-    // let el = document.getElementsByClassName("mat-tab-label-container");
-    // el[0].classList.add("tab");
-  }
+  ngAfterViewInit() {}
 
   onDownloadClick() {}
 }
