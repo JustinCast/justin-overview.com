405e753a86f2e73730d3aa663e0c44a700b17fae
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2019-01-01 21:33:14 -0600

    feat: important changes in ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit, OnInit } from "@angular/core";
 import { ApiManagementService } from "./api-management.service";
+import { Card } from "./models/card";
 //import * as download from "download-pdf";
 @Component({
   selector: "app-root",
@@ -10,23 +11,26 @@ export class AppComponent implements AfterViewInit, OnInit {
   year;
   pdf: string = "../assets/cv.pdf";
   collapsed = true;
-  technologies = {
-    Angularjs: "../assets/",
-    Nodejs: "",
-    "Angular 2+": "",
-    Mongo: "",
-    "MSSQL Server": "",
-    "PostgreSQL": "",
-    "Bootstrap": "",
-    "Angular Material": "",
-    "GraphQL": "",
-    "Apollo": "",
-    "C#": "",
-    "Java": "",
-    "JavaScript": "",
-    "C": "",
-    "C++": "",
-  };
+  technologies: Array<Card> = [
+    { name: "Angularjs", img: "../assets/angularjs.png" },
+    { name: "Python", img: "../assets/python.png" },
+    { name: "Android", img: "../assets/android.png" },
+    { name: "Html5", img: "../assets/html.png" },
+    { name: "Nodejs", img: "../assets/node.png"},
+    { name: "Angular 2+", img: "../assets/angular.png"},
+    { name: "Mongo", img: "../assets/mongo.png"},
+    { name: "MSSQL Server", img: "../assets/sql-server.png"},
+    { name: "PostgreSQL", img: "../assets/postgresql.png"},
+    { name: "Bootstrap", img: "../assets/bootstrap.png"},
+    { name: "Angular Material", img: "../assets/material.png"},
+    { name: "GraphQL", img: "../assets/graphql.png"},
+    { name: "Apollo", img: "../assets/apollo.png"},
+    { name: "C#", img: "../assets/csharp.png"},
+    { name: "Java", img: "../assets/java.png"},
+    { name: "JavaScript", img: "../assets/javascript.png"},
+    { name: "C", img: "../assets/c.png"},
+    { name: "C++", img: "../assets/c++.png"}
+  ];
 
   constructor(private _api: ApiManagementService) {}
 
